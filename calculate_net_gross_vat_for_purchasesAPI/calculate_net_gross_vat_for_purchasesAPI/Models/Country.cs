75946a3dc6a8dce93778889c2638fcfe68660a23
João "Davey" Moreira <joaopedromoreira@gmail.com>
2022-12-09 17:20:51 +0000

    Models for EF added

---
+++ calculate_net_gross_vat_for_purchasesAPI/calculate_net_gross_vat_for_purchasesAPI/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace calculate_net_gross_vat_for_purchasesAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Initials { get; set; } = string.Empty;
+        public virtual List<VATRate> VateRate { get; set; } = new List<VATRate>();
+    }
+}
---
+++ calculate_net_gross_vat_for_purchasesAPI/calculate_net_gross_vat_for_purchasesAPI/Models/VATRate.cs
@@ -0,0 +1,10 @@
+﻿namespace calculate_net_gross_vat_for_purchasesAPI.Models
+{
+    public class VATRate
+    {
+        public int Id { get; set; }
+        public float Rate { get; set; }
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }  = new Country();
+    }
+}
